2c4358bcd900edd62027674bf05950d664fff969
Luis Ramos <120321858+LuisRamosOfficial@users.noreply.github.com>
2024-12-12 00:43:51 +0000

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from 'solid-js';
+
+import styles from './styles/main.module.scss';
+
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+     
+    </div>
+  );
+};
+
+export default App;
